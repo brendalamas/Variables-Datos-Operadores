6cb95965a7be32cf18280e88c978dbffa7ec51f3
brenda <lamasbrenda@hotmail.com>
2021-04-21 19:24:15 -0300

    aprendiendoJs

--- 12 Porcentaje/app12.js
+++ 12 Porcentaje/app12.js
@@ -6,7 +6,7 @@ y luego el porcentaje que se desea obtener del mismo.
 Devolver un mensaje muestre el porcentaje de dicho número.
 */
 
-let numero = prompt ("ingrese un numero");
-let porcentaje = prompt ("ingrese el porcentaje que desea obtener");
+let numero = Number(prompt ("ingrese un numero"));
+let porcentaje = Number(prompt ("ingrese el porcentaje que desea obtener"));
 
 alert (` el porcentaje del numero ingresado es ${((numero * porcentaje)/100)}`);
